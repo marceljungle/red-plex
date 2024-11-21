7553eeb6ba71f8c0ba53d826edd4a49324960aa5
Marcel Dan <gigi.dan2011@gmail.com>
2024-11-21 21:14:55 +0100

    Added datetime column to avoid scanning the entire library each time

--- plex_playlist_creator/plex_manager.py
+++ plex_playlist_creator/plex_manager.py
@@ -1,6 +1,7 @@
 """Module for managing Plex albums and playlists."""
 
 import os
+from datetime import datetime
 from plexapi.server import PlexServer
 from plex_playlist_creator.logger import logger
 from plex_playlist_creator.album_cache import AlbumCache
@@ -23,34 +24,49 @@ class PlexManager:
 
     def populate_album_cache(self):
         """Fetches albums from Plex and saves them to the cache."""
-        logger.info('Creating album cache, this can take a while depending on the library size...')
+        logger.info('Updating album cache...')
         music_library = self.plex.library.section(self.section_name)
-        all_albums = music_library.searchAlbums()
-        album_data = {}
 
-        for album in all_albums:
+        # Determine the latest addedAt date from the existing cache
+        if self.album_data:
+            latest_added_at = max(added_at for _, added_at in self.album_data.values())
+            logger.info('Latest album added at: %s', latest_added_at)
+        else:
+            latest_added_at = datetime.min  # No albums in cache, get all albums
+            logger.info('No existing albums in cache. Fetching all albums.')
+
+        # Fetch albums added after the latest date in cache
+        filters = {"addedAt>>": latest_added_at}
+        new_albums = music_library.searchAlbums(filters=filters)
+        logger.info('Found %d new albums added after %s.', len(new_albums), latest_added_at)
+
+        # Update the album_data dictionary with new albums
+        for album in new_albums:
             tracks = album.tracks()
             if tracks:
                 media_path = tracks[0].media[0].parts[0].file
                 album_folder_path = os.path.dirname(media_path)
-                album_data[int(album.ratingKey)] = album_folder_path
+                added_at = album.addedAt
+                self.album_data[int(album.ratingKey)] = (album_folder_path, added_at)
             else:
                 logger.warning('Skipping album with no tracks: %s', album.title)
 
-        self.album_cache.save_albums(album_data)
-        self.album_data = album_data
+        # Save the updated album data to the cache
+        self.album_cache.save_albums(self.album_data)
 
     def reset_album_cache(self):
         """Resets the album cache by deleting the cache file."""
         self.album_cache.reset_cache()
         self.album_data = {}
+        logger.info('Album cache has been reset.')
 
     def get_rating_keys(self, path):
-        """Returns the rating key if the path matches an album folder."""
-        rating_keys = [key for key, folder_path in self.album_data.items() if path in folder_path]
+        """Returns the rating keys if the path matches an album folder."""
+        rating_keys = [key for key, (folder_path, _)
+                       in self.album_data.items() if path in folder_path]
         if rating_keys:
             logger.info('Matched album folder name: %s, returning rating keys %s...', path,
-                         rating_keys)
+                        rating_keys)
         return rating_keys
 
     def fetch_albums_by_keys(self, rating_keys):
