c8dfab0f708afcff15a2995798bb0799eb62b6f0
Marcel Dan <gigi.dan2011@gmail.com>
2024-12-06 23:29:13 +0100

    Enhance cache_utils with improved directory handling and add unit tests for cross-platform compatibility

--- plex_playlist_creator/cache_utils.py
+++ plex_playlist_creator/cache_utils.py
@@ -1,13 +1,15 @@
+""" This module contains utility functions for working with the cache directory. """
+
 import os
 
 def get_cache_directory():
     """Return the cache directory path based on the OS."""
     if os.name == 'nt':  # Windows
-        return os.path.join(os.getenv('LOCALAPPDATA', os.path.expanduser('~\\AppData\\Local')), 'red-plex')
-    elif os.uname().sysname == 'Darwin':  # macOS
+        return os.path.join(os.getenv('LOCALAPPDATA',
+                                      os.path.expanduser('~\\AppData\\Local')), 'red-plex')
+    if os.uname().sysname == 'Darwin':  # macOS
         return os.path.join(os.path.expanduser('~/Library/Caches'), 'red-plex')
-    else:  # Linux and others
-        return os.path.join(os.path.expanduser('~/.cache'), 'red-plex')
+    return os.path.join(os.path.expanduser('~/.cache'), 'red-plex')# Linux and others
 
 def ensure_directory_exists(directory):
     """Ensure that a directory exists, creating it if necessary."""
