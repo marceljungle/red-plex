cda6d07d582479045c153caba6b5f6871e6b2672
Gigi Marcel Dan <gigimd@ext.zara.com>
2025-05-10 22:21:06 +0200

    Fixed setup.py for correct version generation

--- setup.py
+++ setup.py
@@ -2,18 +2,25 @@
 
 import re
 import pathlib
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages # o from setuptools import setup, find_packages
 
+# Resolve the absolute path to the directory containing setup.py
 here = pathlib.Path(__file__).parent.resolve()
+
+# Read the long description from README.md
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
+# Construct the absolute path to __init__.py within your package
+init_py_path = here / "red_plex" / "__init__.py"
+
 # Read the version from the package's __init__.py
-with open('red_plex/__init__.py', 'r', encoding="utf-8") as f:
+with open(init_py_path, 'r', encoding="utf-8") as f:
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M)
     if version_match:
         version = version_match.group(1)
     else:
-        raise RuntimeError("Unable to find version string.")
+        # It's good to show the path it tried to open for easier debugging
+        raise RuntimeError(f"Unable to find version string in {init_py_path}")
 
 setup(
     name='red_plex',
@@ -24,8 +31,7 @@ setup(
     author='marceljungle',
     author_email='gigi.dan2011@gmail.com',
     url='https://github.com/marceljungle/red-plex',
-    packages=find_namespace_packages(where="red_plex"),
-    package_dir={"": "red_plex"},
+    packages=find_namespace_packages(include=['red_plex*']),
     include_package_data=True,
     install_requires=[
         'plexapi',
@@ -37,7 +43,7 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        red-plex=infrastructure.cli.cli:main
+        red-plex=red_plex.infrastructure.cli.cli:main
     ''',
     classifiers=[
         'Programming Language :: Python :: 3',
